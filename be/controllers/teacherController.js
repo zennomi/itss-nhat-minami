2492d8d1333729258404ea8bf88df32fc656191a
tungnguyensnk <tung.nd194714@sis.hust.edu.vn>
2023-06-11 19:52:24 +0700

    teacher api & seed db

--- be/controllers/teacherController.js
+++ be/controllers/teacherController.js
@@ -33,6 +33,43 @@ const TEACHER = {
             console.log(e);
             return res.status(500).json({message: 'DATABASE_ERROR'});
         }
+    },
+    getTeacher: async (req, res) => {
+        let id = req.params.id || null;
+        try {
+            let kq = await DB.getTeacherInfos(id);
+            if(!kq.id)
+                return res.status(404).json({message: 'NOT_FOUND'});
+            return res.status(200).json(kq);
+        } catch (e) {
+            console.log(e);
+            return res.status(500).json({message: 'DATABASE_ERROR'});
+        }
+    },
+    updateTeacher: async (req, res) => {
+        let teacherId = req.body.teacher_id;
+        if (!teacherId) return res.status(400).json({message: 'MISSING_FIELDS'});
+        let params = {
+            lang_teach: req.body.lang_teach || null,
+            lang_study: req.body.lang_study || null,
+            purpose: req.body.purpose || null,
+            price: req.body.price || null,
+            phone_number: req.body.phone_number || null,
+            resume_url: req.body.resume_url || null,
+            website_url: req.body.website_url || null,
+            facebook_url: req.body.facebook_url || null,
+            instagram_url: req.body.instagram_url || null,
+            linkedin_url: req.body.linkedin_url || null,
+            twitter_url: req.body.twitter_url || null,
+        };
+        try {
+            await DB.updateTeacherInfos(teacherId, params);
+            let kq = await DB.getTeacherInfos(teacherId);
+            return res.status(200).json(kq);
+        } catch (e) {
+            console.log(e);
+            return res.status(500).json({message: 'DATABASE_ERROR'});
+        }
     }
 }
 
