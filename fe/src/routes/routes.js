8e02369be2ecef0a2bf1a50e68241dff2b5d2aff
phtchung <phamthanhchung2001@gmail.com>
2023-06-13 21:55:45 +0700

    add router page bookmark

--- fe/src/routes/routes.js
+++ fe/src/routes/routes.js
@@ -1,5 +1,6 @@
 import * as Pages from '../pages';
 
+
 export const UserRoutes = [];
 
 export const AdminRoutes = [
@@ -15,7 +16,7 @@ export const GuestRoutes = [
 export const NormalRoutes = [
     {path: '/', element: <Pages.Home/>},
     {path: '/search', element: <Pages.Search/>},
-
+    {path: '/bookmark',element: <Pages.Bookmark/>},
     {path: '/tutor/:id', element: <Pages.Tutor/>},
 
 ];
